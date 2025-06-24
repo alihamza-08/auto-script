8a95132e51c48c2cfe1c33ee608469c23a623491
alihamza-08 <alihamza760760@gmail.com>
2025-06-24 06:57:25 -0400

    automated update Tue Jun 24 06:57:25 AM EDT 2025

--- auto.sh
+++ auto.sh
@@ -13,7 +13,16 @@ SLACK_WEBHOOK_URL="https://hooks.slack.com/services/T04D0EKES7J/B092TRQ97GA/KHqX
 
 MESSAGE="🚀 Code pushed to GitHub at $(date)"
 
-curl -X POST -H 'Content-type: application/json' \
+response=$(curl -X POST -H 'Content-type: application/json' \
 --data "{\"text\":\"$MESSAGE\"}" \
+"$SLACK_WEBHOOK_URL")
+
+if [[ "$response" != "ok" ]]; then
+ curl -S -X POST -H 'Content-type: application/json' \
+--data "{\"text\":\"❌ Slack message failed to send. Response: $response\"}" \
 "$SLACK_WEBHOOK_URL"
+else
+	echo "notification sent successfully"
+fi
+
 
