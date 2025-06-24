4260f51c550e02a568edb00514f45ec225870061
alihamza-08 <alihamza760760@gmail.com>
2025-06-24 06:36:20 -0400

    automated update Tue Jun 24 06:36:20 AM EDT 2025

--- auto.sh
+++ auto.sh
@@ -7,3 +7,13 @@ git add .
 git commit -m "automated update $(date) "
 
 git push origin main
+
+# This will be triggered by GitHub via POST request
+SLACK_WEBHOOK_URL="https://hooks.slack.com/services/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"
+
+MESSAGE="🚀 Code pushed to GitHub at $(date)"
+
+curl -X POST -H 'Content-type: application/json' \
+--data "{\"text\":\"$MESSAGE\"}" \
+"$SLACK_WEBHOOK_URL"
+
